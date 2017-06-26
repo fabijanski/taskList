ed2e0b88448680482a03e56d44b8e596dd0357c7
fabijanski <mm.fabijanski@gmail.com>
2017-06-26 02:55:55 +0200

    Improved html, css and js code.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,39 +1,40 @@
+var $restoreButton = $("#restore-button");
 var lastDeleted;
 var indexOfDeleted;
-var popUp = document.getElementById('pop-up'),
-    popuptoggled = false,
-    btntoggled = false;
+var $popUp = $("#pop-up");
+var $newTaskInput = $("#new-task-input");
+var $newTaskButton = $("#new-task-button");
+var isPopupToggled = false;
+var isButtonToggled = false;
 
-///////////////////////////// CHECK OFF CLICKED TASK //////////////////////////
-$("ul").on("click", "li", function(){
+// CHECK OFF CLICKED TASK
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
-/////////////////////// SHOW OR HIDE BUTTON WHEN ACTION ///////////////////////
-var btnVisib = function(event){
-  if(!btntoggled){
-    if(event == "show"){
-      btntoggled = true;
+// SHOW OR HIDE BUTTON WHEN ACTION
+var btnVisib = function(event) {
+  if (!isButtonToggled) {
+    if (event === "show") {
+      isButtonToggled = true;
       showBtn();
     }
-  }else if(event == "hide"){
-      btntoggled = false;
+  } else if (event === "hide") {
+      isButtonToggled = false;
       showBtn();
-  }else{
-      console.log("WARNING");
   }
 }
 
 var showBtn = function() {
-    $("button").fadeToggle(300);
+  $restoreButton.fadeToggle(300);
 }
 
-////////////////////// ACTION AFTER "ENTER" WAS CLICKED ///////////////////////
-$("input[type='text']").on("keypress", function(event){
-  if(event.which === 13){
+// ACTION AFTER "ENTER" WAS CLICKED
+$newTaskInput.on("keypress", function(event) {
+  if (event.which === 13) {
     // grabbing new task from input
     var inputtedText = $(this).val();
-    if(inputtedText !== ""){
+    if (inputtedText !== "") {
       // clearing input
       $(this).val("");
       // show/hide button and alert
@@ -41,26 +42,33 @@ $("input[type='text']").on("keypress", function(event){
       // create a new li and add to ul
       $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + inputtedText + "</li>");
       visib("added");
-    }else{
+    } else {
       visib("error");
     }
   }
 });
 
-//////////////// SHOW AND HIDE THE POP-UP WINDOW WHEN ACTION //////////////////
+// SHOW AND HIDE THE POP-UP WINDOW WHEN ACTION
 var visib = function(e) {
-  if (!popuptoggled) {
+  var errorStyles = {
+    color: "#c82020",
+    height: "40px",
+  }
+  var noErrorStyles = {
+    color: "#62cd19",
+    height: "40px",
+  }
+
+  if (!isPopupToggled) {
     if (e === "error") {
-      popUp.style.color = "#c82020";
-      popUp.style.height = "40px";
-      popUp.textContent = "ENTER A NEW TASK!";
-      popuptoggled = true;
+      $popUp.text("enter a new task!")
+        .css(errorStyles);
+      isPopupToggled = true;
       openPopUp();
     } else if (e === "added") {
-      popUp.style.color = "#62cd19";
-      popUp.style.height = "40px";
-      popUp.textContent = "NEW TASK WAS ADDED!";
-      popuptoggled = true;
+      $popUp.text("new task added!")
+        .css(noErrorStyles);
+      isPopupToggled = true;
       openPopUp();
     } else {
       console.log("WARNING");
@@ -69,14 +77,15 @@ var visib = function(e) {
 }
 
 var openPopUp = function() {
-    $("#pop-up").slideDown(200).delay(1000).slideUp(200);
-    popuptoggled = false;
+  $popUp.slideDown(200).delay(1000).slideUp(200, function() {
+    isPopupToggled = false;
+  });
 }
 
-/////////////////// CLICK ON TRASH ICON TO DELETE THE TASK ////////////////////
-$("ul").on("click", "span", function(event){
+// CLICK ON TRASH ICON TO DELETE THE TASK
+$("ul").on("click", "span", function(event) {
   // lastDeleted = $(this).parent().fadeOut(200, function(){
-  lastDeleted = $(this).parent().animate({width:'toggle'},350, function(){
+  lastDeleted = $(this).parent().animate({width:'toggle'},350, function() {
     $(this).detach();
   });
   indexOfDeleted = lastDeleted.index();
@@ -84,23 +93,23 @@ $("ul").on("click", "span", function(event){
   btnVisib("show");
 });
 
-////////////// CLICK ON BUTTON TO RESTORE LAST DELETED TASK ///////////////////
-$("body").on("click", "button", function(){
-  if(indexOfDeleted === 0){
-    if($("li").length !== 0){
+// CLICK ON BUTTON TO RESTORE LAST DELETED
+$restoreButton.on("click", function() {
+  if (indexOfDeleted === 0) {
+    if ($("li").length !== 0) {
       lastDeleted.fadeIn(200).insertBefore( $("li").eq(indexOfDeleted))
-    }else{
+    } else {
       // when the list is empty
       lastDeleted = lastDeleted.fadeIn(200);
       $("ul").append(lastDeleted);
     }
-  }else{
+  } else {
     lastDeleted.fadeIn(200).insertAfter( $("li").eq(indexOfDeleted-1));
   }
   btnVisib("hide");
 });
 
-////////////////// CLICK ON "+" TO SHOW/HIDE INPUT WINDOW /////////////////////
-$(".fa-plus").click(function(){
-  $("input[type='text']").fadeToggle();
-})
+// CLICK ON "+" TO SHOW/HIDE INPUT WINDOW
+$newTaskButton.on('click', function() {
+  $newTaskInput.fadeToggle();
+});
